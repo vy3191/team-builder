568c14137fb27b965692ccfa9a053895ed7e2605
venkyy2019 <vekyy2019@gmail.com>
2019-09-10 19:53:08 -0400

    Started fetching data to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Data from './data';
+console.log(Data)
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+     const getData =async () => {
+       const promiseValue = Promise.resolve(Data);
+             promiseValue.then( response => {
+                 console.log(response)
+             })
+                        
+     }
+     getData();
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form Management-React</h1>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: "venky",
+    email: "venky@gmail.com",
+    role: "backend engineer"
+
+  },
+  {
+    name: "venkatesh",
+    email: "venkatesh@gmail.com",
+    role: "front engineer"
+  },
+  {
+    name: "venkateshwarlu",
+    email: "yvenkesh@gmail.com",
+    role: "Full stack developer"
+
+  }
+
+];
